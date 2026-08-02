75259381b6c2cc8f0fa67c62f00538d4cc5f9dab
Satyanarayan Rout <101426863+kunal101994@users.noreply.github.com>
2026-08-02 18:55:18 +0000

    data_types.js

---
+++ basic/data_types.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all js code as newer version
+// alert(3+3) // we are using nodejs, not browser
+
+
+let name = "Satya" // string
+let age = 29 // number
+let isLoggedIn = false // boolean
+
+// number renge - 2 to power 32 // memory address pe milta hai
+// bigint - trading, stock market, crypto currency, data science
+// string - single or double quotes, template literals
+// boolean - true or false
+// null - standlone value/ empty value
+// undefinedc - value is not defined
+
+console.log(typeof null);
+console.log(typeof undefined);
