fb49a8b2cc653d1092f906aa69bde96eb0822d3b
Satyanarayan Rout <101426863+kunal101994@users.noreply.github.com>
2026-08-04 17:27:11 +0000

    conversionOperator.js

---
+++ basic/conversionOperator.js
@@ -0,0 +1,33 @@
+// let score = "33abc"
+
+// let score = null
+// let score = undefined
+// let score = true
+// let score = false
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN -> Not a Number
+// true =>1; false=> 0
+
+
+// let isLoggedIn = 1
+// let isLoggedIn = "Satya"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0=> false
+// "" => false
+// "satya" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof(stringNumber))
